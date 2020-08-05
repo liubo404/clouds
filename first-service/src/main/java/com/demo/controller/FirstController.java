e510b3a4c06292202b8de1243bd29663e8ee37f4
liubo <1091643978@qq.com>
2020-08-05 09:07:52 +0800

    cloud init

---
+++ first-service/src/main/java/com/demo/controller/FirstController.java
@@ -0,0 +1,45 @@
+package com.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/employee")
+public class FirstController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+//    @Bean
+//    public AlwaysSampler alwaysSampler() {
+//        return new AlwaysSampler();
+//    }
+
+    private static final Logger LOG = Logger.getLogger(FirstController.class.getName());
+
+
+    @GetMapping("/message")
+    public String test() {
+        LOG.info("Inside zipkinService 1..");
+
+        String response = (String) restTemplate.exchange("http://localhost:8082/consumer/message",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+                }).getBody();
+//        return "Hi...";
+
+        return "Hello JavaInUse Called in First Service" + response;
+    }
+}
